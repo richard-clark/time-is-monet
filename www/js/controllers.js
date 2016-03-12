561b33159a822ee55278cf14fb72148e856d799b
richard-clark <rctheslimer@gmail.com>
2016-03-12 16:37:34 -0500

    It works!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,13 @@
 angular.module('starter.controllers', ['ngCordovaBeacon'])
 
 .controller("IBeaconsController", function($scope, $rootScope, $ionicPlatform, $cordovaBeacon) {
- 
+
     $scope.beacons = {};
- 
+
     $ionicPlatform.ready(function() {
- 
+
         $cordovaBeacon.requestWhenInUseAuthorization();
- 
+
         $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
             var uniqueBeaconKey;
             for(var i = 0; i < pluginResult.beacons.length; i++) {
@@ -16,8 +16,8 @@ angular.module('starter.controllers', ['ngCordovaBeacon'])
             }
             $scope.$apply();
         });
- 
-        $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("pms", "f7826da6-4fa2-4e98-8024- bc5b71e0893e"));
+
+        $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("pms", "f7826da6-4fa2-4e98-8024-bc5b71e0893e"));
  
     });
 })
